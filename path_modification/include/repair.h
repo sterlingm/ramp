3833ef478f1c44ffeefeee2ddc5f327543896d50
sterlingm <sterlingmcleod9@gmail.com>
2018-03-12 13:34:03 -0400

    Add Repair operator files in path_mod

---
+++ path_modification/include/repair.h
@@ -0,0 +1,25 @@
+#ifndef REPAIR_H
+#define REPAIR_H
+#include "utility.h"
+#include "constraints.h"
+#include "range.h"
+
+class Repair {
+  public:
+    Repair() {}
+    Repair(const ramp_msgs::Path p);
+
+    const ramp_msgs::Path perform();
+    const std::vector<double> getNewPosition(const double value, const double other_value, const double bound, const double theta, const double r, bool x_dim) const;
+
+    ramp_msgs::Path path_;
+    double dir_;
+    double dist_;
+    double r_;
+
+    Utility utility_;
+  private:
+    Constraints checkConstraints_;
+};
+
+#endif
