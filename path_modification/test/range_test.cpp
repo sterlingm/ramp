329518903d6199ee00f4cf11bdca536b8982002a
Nolan <njorda13@uncc.edu>
2019-02-19 15:07:58 -0500

    added range_test

---
+++ path_modification/test/range_test.cpp
@@ -0,0 +1,25 @@
+
+#include <gtest/gtest.h>
+#include "../include/range.h"
+
+TEST(TestSuite, randtest)
+{
+
+  // make range object // 
+  Range myrange(0,1);
+
+  // call random // 
+  float num = myrange.random();
+  
+  // check if num between min and max //
+  EXPECT_GT(num , 0);
+
+
+
+}// end TEST
+
+int main(int argc, char** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
