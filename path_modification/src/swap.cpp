8d1ccdfc3e5b7bb38a88144c567c17d116bb6611
George Hall <hall.george212@gmail.com>
2019-04-01 12:26:07 -0400

    Added overloaded method for swap.cpp to use nav_msgs and modified swap.h

--- path_modification/src/swap.cpp
+++ path_modification/src/swap.cpp
@@ -30,3 +30,32 @@ const ramp_msgs::Path Swap::perform()
 
   return path_;
 }
+
+
+/** Swap does NOT swap the velocity values! Only the positions! */
+const nav_msgs::Path Swap::navPerform() 
+{
+
+  if(navPath_.poses.size() > 3) 
+  {
+    unsigned int i_Point1 = rand() % (navPath_.poses.size()-2) + 1;
+    unsigned int i_Point2; 
+    
+    // Make sure the two configurations are different
+    do 
+    {
+      i_Point2 = rand() % (navPath_.poses.size()-2) + 1;
+    } while(i_Point1 == i_Point2);
+
+    //std::cout<<"\ni_knotPoint1: "<<i_knotPoint1<<" i_knotPoint2: "<<i_knotPoint2;
+    
+    // Swap the knot points' positions
+    geometry_msgs::PoseStamped temp = navPath_.poses[i_Point1];
+    //std::vector<double> temp = navPath_.poses.at(i_Point1);
+    navPath_.poses[i_Point1] = navPath_.poses[i_Point2];
+    navPath_.poses[i_Point2] = temp;
+
+  }
+
+  return navPath_;
+}
